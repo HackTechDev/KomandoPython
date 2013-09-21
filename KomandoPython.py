3f7f327421c8f952abd8a3f0c47265aec2b347e8
LeSanglier <lesanglierdesardennes@gmail.com>
2013-09-21 18:33:31 +0200

    Switch level

--- KomandoPython.py
+++ KomandoPython.py
@@ -202,6 +202,7 @@ class Player(pygame.sprite.Sprite):
 class Level():
     # Constructor function
     def __init__(self, filename):
+
         # Load background
         file = open("maps/"+filename+"/background.txt", "r")
         line_list = file.readlines()
@@ -213,7 +214,6 @@ class Level():
             line = line[:-1]
             tiles = line.split(':')
             for tile in tiles:
-                print str(posx) + " " + str(posy) + " " + tile
                 if tile == "01":
                     ground = Ground("brown_paving.png", posx, posy, 32, 32)
                     ground_list.add(ground)
@@ -224,7 +224,7 @@ class Level():
             posy = posy + 1
             posx = 0 
             
-         # Load wall
+        # Load wall
         file = open("maps/"+filename+"/wall.txt", "r")
         line_list = file.readlines()
         file.close()
@@ -235,7 +235,6 @@ class Level():
             line = line[:-1]
             tiles = line.split(':')
             for tile in tiles:
-                print str(posx) + " " + str(posy) + " " + tile
                 if tile == "01":
                     wall = Wall("int_wall_bricks.png", posx, posy, 32, 32)
                     all_sprites_list.add(wall)
@@ -243,7 +242,7 @@ class Level():
             posy = posy + 1
             posx = 0 
             
-         # Load item
+        # Load item
         file = open("maps/"+filename+"/item.txt", "r")
         line_list = file.readlines()
         file.close()
@@ -254,7 +253,6 @@ class Level():
             line = line[:-1]
             tiles = line.split(':')
             for tile in tiles:
-                print str(posx) + " " + str(posy) + " " + tile
                 if tile == "01":
                     item = Item("toys.png", posx, posy, 32, 32)
                     item_list.add(item)
@@ -262,6 +260,17 @@ class Level():
                 posx = posx + 1
             posy = posy + 1
             posx = 0 
+
+    def empty(self):
+        for sprite in all_sprites_list:
+            all_sprites_list.remove(sprite)
+       
+        for item in item_list:
+            item_list.remove(item)
+    
+        for ground in ground_list:
+            ground_list.remove(ground)
+
  
 # Call this function so the Pygame library can initialize itself
 pygame.init()
@@ -284,19 +293,17 @@ player = Player(32, 64)
 movingsprites = pygame.sprite.RenderPlain()
 movingsprites.add(player)
 
-# Make the walls. (x_pos, y_pos, width, height)
-all_sprites_list=pygame.sprite.RenderPlain()
+all_sprites_list = pygame.sprite.RenderPlain()
 
-# List of each item in the game
 item_list = pygame.sprite.RenderPlain()
 
+ground_list = pygame.sprite.RenderPlain()
+ 
 # List of each bullet
 bullet_list = pygame.sprite.RenderPlain()
 
-ground_list = pygame.sprite.RenderPlain()
-
 # Load level
-map1 = Level("01")
+level = Level("01")
 
 clock = pygame.time.Clock()
 
@@ -319,6 +326,12 @@ while done == False:
             done=True
 
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                level.empty()
+                level = Level("01")				
+            if event.key == pygame.K_z:
+                level.empty()
+                level = Level("02")	
             if event.key == pygame.K_LEFT:
                 player.changespeed(-speed,0)
                 direction = 4
