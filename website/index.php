c91120e375f7f0319cc9cfa7a46d021cfe9a8cb1
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2014-08-13 20:47:40 +0200

    Add files website

---
+++ website/index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Commando Ninja : Infiltration Zombi</title>
+<style type="text/css">
+#ninja {
+  background-image: url(ninja.gif);
+  background-repeat: no-repeat;
+  padding: 0;
+  width: 397px;
+  height: 685px;
+}
+
+#ninjaanimated {
+  background-image: url(ninjaredeyeanimated.gif);
+  background-repeat: no-repeat;
+  padding: 0;
+  width: 163px;
+  height: 141px;
+  position: relative; 
+  top: 0px; 
+  left: 59px;
+}
+</style>
+</head>
+<body>
+<center>
+<h1>Commando Ninja : Infiltration Zombi</h1>
+<br/>
+<div id="ninja">
+<div id="ninjaanimated">
+</div>
+</div>
+
+<br/>
+Téléchargement : <a href="https://github.com/Nekrofage/KomandoPython">Code source</a><br/>
+<br/>
+Capture d'écran : <br />
+<br />
+<img src="captureecran/capture007.png" border="0" width="600px"><br />
+<br />
+Vidéo :<br />
+<br />
+<iframe width="420" height="315" src="//www.youtube.com/embed/5J2z3NgDwz0" frameborder="0" allowfullscreen></iframe>
+</center>
+</body>
+</html>
+
